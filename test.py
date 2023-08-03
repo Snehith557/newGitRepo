5767ae0fb8bd51a27605c002e15281e919af4a46
Snehith557 <88615935+Snehith557@users.noreply.github.com>
2023-08-03 22:56:21 +0530

    adding a test.py file

---
+++ test.py
@@ -0,0 +1,11 @@
+# hey started to learn git and github 
+# lets see how it goes
+# i am starting with a small program in python
+
+def findGit(string):
+
+    print(string)
+    return
+
+
+findGit("Hello World")
